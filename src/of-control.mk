977c9a71f5a5af371ca9cb7669d1ad535bb76a7d
Markus Mützel <markus.muetzel@gmx.de>
2021-06-24 20:36:42 +0200

    of-control: update to v3.3.0
    * src/of-control.mk: Update version and checksum.

--- src/of-control.mk
+++ src/of-control.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-control
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2.0
-$(PKG)_CHECKSUM := 46e8e04ea456f871d74049fed2db0840e6d7dc8a
+$(PKG)_VERSION  := 3.3.0
+$(PKG)_CHECKSUM := 1d5d711d3c2f8b4957cc4930e42672ed6974debc
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := control-$($(PKG)_VERSION)
 $(PKG)_FILE     := control-$($(PKG)_VERSION).tar.gz
