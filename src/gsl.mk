aca6b8f927181dce5ccf3ba26040a7d58eae9f28
John Donoghue <john.donoghue@ieee.org>
2021-07-05 07:25:54 -0400

    * src/gsl.mk: update to v2.7

--- src/gsl.mk
+++ src/gsl.mk
@@ -3,8 +3,8 @@
 
 PKG             := gsl
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.6
-$(PKG)_CHECKSUM := 9273164b6bdf60d0577518a1c1310eff6659e3dd
+$(PKG)_VERSION  := 2.7
+$(PKG)_CHECKSUM := 29179db0d746f422bb0ceca2cbda4de107a2c651
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
