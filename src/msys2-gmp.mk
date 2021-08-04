bca087ab9f11b3a074c0327ceac6b2b391f77665
Kai T. Ohlhus <k.ohlhus@gmail.com>
2021-08-04 12:48:40 +0900

    msys2-gmp: Update to version 6.2.1-1 (bug #61008).
    * src/msys2-gmp.mk, src/src-msys2-gmp.mk: Update version and checksums. Strip
      trailing whitespaces.

--- src/msys2-gmp.mk
+++ src/msys2-gmp.mk
@@ -4,16 +4,16 @@
 PKG             := msys2-gmp
 $(PKG)_NAME     := gmp
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.2.0-1
-$(PKG)_x86_64_CS := aaa5e44a168ea7afe922f59cef94274b491a0e2d
-$(PKG)_i686_CS  := 6aef4305181c56241feb5a767c19f34dbefdc7b3
+$(PKG)_VERSION  := 6.2.1-1
+$(PKG)_x86_64_CS := ae059cef3d83843567401014daaa97ba8be64b79
+$(PKG)_i686_CS  := 74eb22780e0df1c4156d23a4988a23aa5087cdc4
 $(PKG)_CS       := $($(PKG)_$(MSYS2_ARCH)_CS)
 $(PKG)_CHECKSUM := $($(PKG)_CS)
-$(PKG)_SUBDIR   := 
-$(PKG)_FILE     := $($(PKG)_NAME)-$($(PKG)_VERSION)-$(MSYS2_ARCH).pkg.tar.xz
+$(PKG)_SUBDIR   :=
+$(PKG)_FILE     := $($(PKG)_NAME)-$($(PKG)_VERSION)-$(MSYS2_ARCH).pkg.tar.zst
 $(PKG)_URL      := $(MSYS2_URL)/$($(PKG)_FILE)
 
-$(PKG)_DEPS     := 
+$(PKG)_DEPS     :=
 
 define $(PKG)_UPDATE
     $(MSYS2_PKG_UPDATE)
