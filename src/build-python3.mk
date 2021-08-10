406150da8661b5a00b60c9262863a89c4bf73865
John W. Eaton <jwe@octave.org>
2021-08-10 12:33:10 -0400

    use system libffi for build-python3 (bug #61024)
    * build-python3.mk: Assume libffi is installed on build system.

--- src/build-python3.mk
+++ src/build-python3.mk
@@ -8,7 +8,9 @@ $(PKG)_CHECKSUM := 3bafa40df1cd069c112761c388a9f2e94b5d33dd
 $(PKG)_SUBDIR   := Python-$($(PKG)_VERSION)
 $(PKG)_FILE     := Python-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://www.python.org/ftp/python/$($(PKG)_VERSION)/$($(PKG)_FILE)
-$(PKG)_DEPS     := build-libffi
+
+## For now, assume that libffi is installed on the build system.
+$(PKG)_DEPS     := # build-libffi
 ifneq ($(USE_SYSTEM_GCC),yes)
   $(PKG)_DEPS     += build-gcc
 endif
@@ -24,9 +26,8 @@ define $(PKG)_BUILD
     PKG_CONFIG_PATH='$(BUILD_PKG_CONFIG_PATH)' \
     LD_LIBRARY_PATH='$(LD_LIBRARY_PATH)' \
     '$(1)/configure' \
-      --prefix='$(BUILD_TOOLS_PREFIX)' \
-     
-        
+      --prefix='$(BUILD_TOOLS_PREFIX)'
+
   $(MAKE) -C '$(1)/.build' -j '$(JOBS)'
   $(MAKE) -C '$(1)/.build' -j 1 install
 endef
