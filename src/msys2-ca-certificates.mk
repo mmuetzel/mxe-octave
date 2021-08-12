af2631316d8d4c2c059c690ea880ed19925646d1
John Donoghue <john.donoghue@ieee.org>
2021-08-12 14:44:31 -0400

    * src/msys2-ca-certificates.mk: add post install script

--- src/msys2-ca-certificates.mk
+++ src/msys2-ca-certificates.mk
@@ -13,12 +13,21 @@ $(PKG)_SUBDIR   :=
 $(PKG)_FILE     := $($(PKG)_NAME)-$($(PKG)_VERSION)-$(MSYS2_ARCH).pkg.tar.zst
 $(PKG)_URL      := $(MSYS2_URL)/$($(PKG)_NAME)-$($(PKG)_VERSION)-any.pkg.tar.zst
 
-$(PKG)_DEPS     := 
+$(PKG)_DEPS     := msys2-openssl
 
 define $(PKG)_UPDATE
     $(MSYS2_PKG_UPDATE)
 endef
 
 define $(PKG)_BUILD
+    # we need do create a post install for the certs
+    mkdir -p "$(1)/etc/post-install"
+    cat "$(1)/.INSTALL" > "$(1)/etc/post-install/07-ca-certificates.post"
+    echo "" >> "$(1)/etc/post-install/07-ca-certificates.post"
+    echo "if [ ! -d /etc/pacman.d/gnupg ]" >> "$(1)/etc/post-install/07-ca-certificates.post"
+    echo "then" >> "$(1)/etc/post-install/07-ca-certificates.post"
+    echo "  (cd / && post_install)" >> "$(1)/etc/post-install/07-ca-certificates.post"
+    echo "fi" >> "$(1)/etc/post-install/07-ca-certificates.post"
+
     $(MSYS2_PKG_BUILD)
 endef
