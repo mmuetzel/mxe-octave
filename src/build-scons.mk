7fa354e91a6f45baa6b2b7e51acb8a71532db66a
Markus Mützel <markus.muetzel@gmx.de>
2021-08-21 20:49:11 +0200

    build-scons.mk: Update to version 4.1.0.post1.
    * src/build-scons.mk: Update version, checksum, subdirectory name, and download
      url.

--- src/build-scons.mk
+++ src/build-scons.mk
@@ -3,16 +3,12 @@
 
 PKG             := build-scons
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.1.2
-$(PKG)_CHECKSUM := d7541717d503266b37004a33246b6688fe87dec0
-$(PKG)_SUBDIR   := scons-$($(PKG)_VERSION)
+$(PKG)_VERSION  := 4.1.0.post1
+$(PKG)_CHECKSUM := 47bb89c546804fd76724fd206817ae9a19d4f2bd
+$(PKG)_SUBDIR   := SCons-$($(PKG)_VERSION)
 $(PKG)_FILE     := scons-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := http://prdownloads.sourceforge.net/scons/$($(PKG)_FILE)
-ifeq ($(MXE_NATIVE_MINGW_BUILD),yes)
-    $(PKG)_DEPS     := 
-else
-    $(PKG)_DEPS     := build-python3
-endif
+$(PKG)_URL      := https://sourceforge.net/projects/scons/files/scons/$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_DEPS     := build-python3
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- https://sourceforge.net/projects/scons/files/scons/ | \
