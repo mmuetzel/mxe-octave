56cab22cd3389e04866b4774ebcfc8d409c68af6
Markus Mützel <markus.muetzel@gmx.de>
2021-10-09 19:25:04 +0200

    of-sparsersb: Don't use internal functions (bug #61320).
    * src/of-sparsersb-1-no-internal-mex-fcns.patch: Add patch.
    * dist-files.mk: Add new file to list.

--- dist-files.mk
+++ dist-files.mk
@@ -533,6 +533,7 @@ SRC_FILES_1 := \
   of-signal-1-deprecated-warnings.patch \
   of-signal.mk \
   of-sockets.mk \
+  of-sparsersb-1-no-internal-mex-fcns.patch \
   of-sparsersb.mk \
   of-specfun-1-deprecated.patch \
   of-specfun.mk \
