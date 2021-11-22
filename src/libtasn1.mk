eaa764cb3a086220335525996a08eaec01f55aff
John Donoghue <john.donoghue@ieee.org>
2021-11-22 07:44:07 -0500

    * src/libtasn1.mk: update to v4.18

--- src/libtasn1.mk
+++ src/libtasn1.mk
@@ -1,8 +1,8 @@
 # This file is part of MXE. See LICENSE.md for licensing information.
 
 PKG             := libtasn1
-$(PKG)_VERSION  := 4.17.0
-$(PKG)_CHECKSUM := 327c3f1305a502dbceb098838a9baed2f0449ddd
+$(PKG)_VERSION  := 4.18.0
+$(PKG)_CHECKSUM := 1665249ce66e64958e8c61d5bdc6c1e9f3623888
 $(PKG)_SUBDIR   := libtasn1-$($(PKG)_VERSION)
 $(PKG)_FILE     := libtasn1-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://ftp.gnu.org/gnu/libtasn1/$($(PKG)_FILE)
