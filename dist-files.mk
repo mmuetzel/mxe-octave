62987f58c27652d351430e00f2e34786cf363bb3
Markus Mützel <markus.muetzel@gmx.de>
2021-11-28 20:28:28 +0100

    dist-files.mk: Add change missing from last commit.

--- dist-files.mk
+++ dist-files.mk
@@ -505,6 +505,7 @@ SRC_FILES_1 := \
   of-instrument-control.mk \
   of-interval-1-cross-fixes.patch \
   of-interval-2-subsref.patch \
+  of-interval-3-error_state.patch \
   of-interval.mk \
   of-io.mk \
   of-linear-algebra.mk \
