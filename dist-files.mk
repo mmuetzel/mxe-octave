8be7bba52cb4102bdc3b2452f639bfbf6fccf5c7
Markus Mützel <markus.muetzel@gmx.de>
2022-01-26 19:14:36 +0100

    of-sparsersb: Don't force -std=gnu++11. We prefer a newer standard now.
    * src/of-sparsersb-2-no-cxx11.patch: Add new patch.
    * dist-files.mk: Include new patch.

--- dist-files.mk
+++ dist-files.mk
@@ -539,6 +539,7 @@ SRC_FILES_1 := \
   of-signal.mk \
   of-sockets.mk \
   of-sparsersb-1-no-internal-mex-fcns.patch \
+  of-sparsersb-2-no-cxx11.patch \
   of-sparsersb.mk \
   of-specfun-1-deprecated.patch \
   of-specfun.mk \
