b376b497ca7670457354df15815b8d6d0c74dd8b
John Donoghue <john.donoghue@ieee.org>
2022-02-06 10:06:00 -0500

    * src/libgpg_error.mk: update to v1.44

--- src/libgpg_error.mk
+++ src/libgpg_error.mk
@@ -3,8 +3,8 @@
 
 PKG             := libgpg_error
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.43
-$(PKG)_CHECKSUM := e71b77e8e32023dfd9cb06504942aa8e028c8795
+$(PKG)_VERSION  := 1.44
+$(PKG)_CHECKSUM := c16ab473d1e558ba1712c77ace38bd03e467cbb0
 $(PKG)_SUBDIR   := libgpg-error-$($(PKG)_VERSION)
 $(PKG)_FILE     := libgpg-error-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://ftp.gnupg.org/gcrypt/libgpg-error/$($(PKG)_FILE)
