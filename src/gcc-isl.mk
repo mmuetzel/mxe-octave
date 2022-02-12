fb43d5aac61cb9cb528edf6564e13e64ad5fa696
Markus Mützel <markus.muetzel@gmx.de>
2022-02-12 12:03:29 +0100

    isl: Switch to official mirror.
    * src/gcc-isl.mk, src/isl.mk: Use new "official" mirror.
    See: https://groups.google.com/g/isl-development/c/JGaMo2VUu_8/m/wVTUVZrrAQAJ

--- src/gcc-isl.mk
+++ src/gcc-isl.mk
@@ -4,10 +4,10 @@
 PKG             := gcc-isl
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 0.22.1
-$(PKG)_CHECKSUM := ce66c4415906bf2c16a8f09f31383f4fe0991531
-$(PKG)_SUBDIR   := isl-isl-$($(PKG)_VERSION)-788faca
+$(PKG)_CHECKSUM := e01a78ddf1f756a7e2f94fc1baa8abacae147d4d
+$(PKG)_SUBDIR   := isl-$($(PKG)_VERSION)
 $(PKG)_FILE     := isl-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://repo.or.cz/isl.git/snapshot/$($(PKG)_FILE)
+$(PKG)_URL      := https://libisl.sourceforge.io/$($(PKG)_FILE)
 $(PKG)_DEPS     := build-libtool gcc-gmp
 
 define $(PKG)_UPDATE
@@ -16,12 +16,11 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./autogen.sh
-    mkdir '$(1).build'
-    cd    '$(1).build' && '$(1)/configure' \
+    cd '$(1)' && mkdir '$(1)/.build'
+    cd '$(1)/.build' && '$(1)/configure' \
         --prefix='$(BUILD_TOOLS_PREFIX)' \
         --disable-shared \
         --with-gmp-prefix='$(BUILD_TOOLS_PREFIX)'
-    $(MAKE) -C '$(1).build' -j '$(JOBS)'
-    $(MAKE) -C '$(1).build' -j 1 install
+    $(MAKE) -C '$(1)/.build' -j '$(JOBS)'
+    $(MAKE) -C '$(1)/.build' -j 1 install
 endef
--- src/isl.mk
+++ src/isl.mk
@@ -4,10 +4,10 @@
 PKG             := isl
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 0.22.1
-$(PKG)_CHECKSUM := ce66c4415906bf2c16a8f09f31383f4fe0991531
-$(PKG)_SUBDIR   := isl-$(PKG)-$($(PKG)_VERSION)-788faca
+$(PKG)_CHECKSUM := e01a78ddf1f756a7e2f94fc1baa8abacae147d4d
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://repo.or.cz/isl.git/snapshot/$($(PKG)_FILE)
+$(PKG)_URL      := https://libisl.sourceforge.io/$($(PKG)_FILE)
 $(PKG)_DEPS     := build-gcc gmp
 
 $(PKG)_EXTRA_MAKE_FLAGS := LDFLAGS='-no-undefined'
@@ -21,11 +21,12 @@ define $(PKG)_UPDATE
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && ./autogen.sh && ./configure \
+    cd '$(1)' && mkdir '$(1)/.build'
+    cd '$(1)/.build' && '$(1)/configure' \
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)' \
         --with-gmp-prefix='$(HOST_PREFIX)'
-    $(MAKE) -C '$(1)' -j '$(JOBS)' $($(PKG)_EXTRA_MAKE_FLAGS)
-    $(MAKE) -C '$(1)' -j '$(JOBS)' $($(PKG)_EXTRA_MAKE_FLAGS) install
+    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' $($(PKG)_EXTRA_MAKE_FLAGS)
+    $(MAKE) -C '$(1)/.build' -j '$(JOBS)' $($(PKG)_EXTRA_MAKE_FLAGS) install
 endef
