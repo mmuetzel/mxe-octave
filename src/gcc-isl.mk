8a906094e6b7561783ea253933c91c3b4b45ffc8
Markus Mützel <markus.muetzel@gmx.de>
2022-02-13 11:21:22 +0100

    isl: Update to version 0.24.
    * src/gcc-isl.mk, src/isl.mk: Update version and checksum.

--- src/gcc-isl.mk
+++ src/gcc-isl.mk
@@ -3,8 +3,8 @@
 
 PKG             := gcc-isl
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.22.1
-$(PKG)_CHECKSUM := e01a78ddf1f756a7e2f94fc1baa8abacae147d4d
+$(PKG)_VERSION  := 0.24
+$(PKG)_CHECKSUM := b0c46cc1c4fb1658def8d3c4c702a01049ea795e
 $(PKG)_SUBDIR   := isl-$($(PKG)_VERSION)
 $(PKG)_FILE     := isl-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://libisl.sourceforge.io/$($(PKG)_FILE)
