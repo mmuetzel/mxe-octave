c31606861d2586221ac77e60f69d9e6f8458064e
John Donoghue <john.donoghue@ieee.org>
2022-03-30 15:06:05 -0400

    * src/gdcm.mk: update to v3.0.11

--- src/gdcm.mk
+++ src/gdcm.mk
@@ -3,8 +3,8 @@
 
 PKG             := gdcm
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.10
-$(PKG)_CHECKSUM := dd3543fde27351744cd31b2a80b6800d7810047d
+$(PKG)_VERSION  := 3.0.11
+$(PKG)_CHECKSUM := 26475e4e373bd548e2f838fb6a06feb7104bb306
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG) 3.x/GDCM $($(PKG)_VERSION)/$($(PKG)_FILE)
