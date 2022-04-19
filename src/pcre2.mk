110ffa9ea6ab2806fd7a735e99a12f45b5c78946
John Donoghue <john.donoghue@ieee.org>
2022-04-19 09:50:29 -0400

    * src/pcre2.mk: update to v10.40

--- src/pcre2.mk
+++ src/pcre2.mk
@@ -3,8 +3,8 @@
 
 PKG             := pcre2
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 10.39
-$(PKG)_CHECKSUM := 9cf3ec88cd68dc76354eb0568738c47b57377042
+$(PKG)_VERSION  := 10.40
+$(PKG)_CHECKSUM := 5a433f92b29083d0d8ccd4ec56e3afbe1fa09863
 $(PKG)_SUBDIR   := pcre2-$($(PKG)_VERSION)
 $(PKG)_FILE     := pcre2-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://github.com/PhilipHazel/$(PKG)/releases/download/$(PKG)-$($(PKG)_VERSION)/$($(PKG)_FILE)
