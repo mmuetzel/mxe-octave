53fb40eb935524bd9c48a3d5effa8be4d114afaa
John Donoghue <john.donoghue@ieee.org>
2022-04-22 10:27:33 -0400

    gl2ps: dont install docs to basedir, remove unneeded manual install of files
    * src/gl2ps-2-docpath.patch: dont set doc dir to . when cross compiling
    
    * src/gl2ps.mk: remove unrequired mingw manual install of files

--- src/gl2ps.mk
+++ src/gl2ps.mk
@@ -73,12 +73,6 @@ else
                 $(INSTALL) -m644 '$(1)/gl2ps.h' '$(3)$(HOST_INCDIR)'; \
             else \
                 $(MAKE) -C '$(1)' -j 1 VERBOSE=1 DESTDIR='$(3)' install; \
-                if [ $(MXE_SYSTEM) = mingw ]; then \
-                    echo "Install dll"; \
-                    $(INSTALL) -d '$(3)$(HOST_BINDIR)'; \
-                    $(INSTALL) '$(3)$(HOST_LIBDIR)/libgl2ps.dll' '$(3)$(HOST_BINDIR)/'; \
-                    rm -f '$(3)$(HOST_LIBDIR)/libgl2ps.dll'; \
-                fi; \
             fi
         endef
     else
