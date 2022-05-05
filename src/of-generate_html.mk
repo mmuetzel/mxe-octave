2b873a1802c68c6ee41de306e7d0847f53387622
John Donoghue <john.donoghue@ieee.org>
2022-05-05 14:15:35 -0400

    * src/of-generate_html.mk: update to v0.3.3

--- src/of-generate_html.mk
+++ src/of-generate_html.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-generate_html
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.3.2
-$(PKG)_CHECKSUM := 488faab3283d7bccbf4d69bd67c4f6905a034feb
+$(PKG)_VERSION  := 0.3.3
+$(PKG)_CHECKSUM := 46843aec87991f1c48931d7054721e382850a732
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := generate_html-$($(PKG)_VERSION)
 $(PKG)_FILE     := generate_html-$($(PKG)_VERSION).tar.gz
