d716c21b6c125601a6b8b61c22cce3a8a087757e
John Donoghue <john.donoghue@ieee.org>
2022-05-31 08:20:35 -0400

    * src/freetds.mk: update to v1.3.11

--- src/freetds.mk
+++ src/freetds.mk
@@ -3,8 +3,8 @@
 
 PKG             := freetds
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.3.10
-$(PKG)_CHECKSUM := d469b5a809e5c0caa867ae4fd84edafdfb7c4141
+$(PKG)_VERSION  := 1.3.11
+$(PKG)_CHECKSUM := b8742f124506cda003e2d8a288aa7fdb9901c892
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := ftp://ftp.freetds.org/pub/$(PKG)/stable/$($(PKG)_FILE)
