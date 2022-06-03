28a4ab00607b9c9d877082b1f62818ccf2ee8917
John Donoghue <john.donoghue@ieee.org>
2022-06-03 13:54:58 -0400

    * src/nettle.mk: update to v3.8

--- src/nettle.mk
+++ src/nettle.mk
@@ -3,8 +3,8 @@
 
 PKG             := nettle
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.7.3
-$(PKG)_CHECKSUM := 9adfadd4ae4104c8aceb38bf16064d65d7edbcce
+$(PKG)_VERSION  := 3.8
+$(PKG)_CHECKSUM := 17954e8e4c980a124e9e1aa24d87451d24a65a7b
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.lysator.liu.se/~nisse/archive/$($(PKG)_FILE)
