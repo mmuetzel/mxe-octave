998e7f09a90439fcb5ba9ccbfa34dd9a97ce7170
John Donoghue <john.donoghue@ieee.org>
2022-06-15 10:00:13 -0400

    * src/isl.mk: use gcc webpage for update versions

--- src/isl.mk
+++ src/isl.mk
@@ -8,14 +8,15 @@ $(PKG)_CHECKSUM := b0c46cc1c4fb1658def8d3c4c702a01049ea795e
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://libisl.sourceforge.io/$($(PKG)_FILE)
+$(PKG)_URL_2    := https://gcc.gnu.org/pub/gcc/infrastructure/$($(PKG)_FILE)
 $(PKG)_DEPS     := build-gcc gmp
 
 $(PKG)_EXTRA_MAKE_FLAGS := LDFLAGS='-no-undefined'
 
 # stick to tested versions from gcc
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'https://repo.or.cz/isl.git/refs' | \
-    $(SED) -n 's,.*>tags/isl-\([0-9][^<]*\).*,\1,p' | \
+    $(WGET) -q -O- 'https://gcc.gnu.org/pub/gcc/infrastructure/' | \
+    $(SED) -n 's,.*isl-\([0-9][^>]*\)\.tar.*,\1,p' | \
     $(SORT) -V |
     tail -1
 endef
