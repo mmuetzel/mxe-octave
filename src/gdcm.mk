69442bdeefb040088f8d5a715badc0d9fc1da554
John Donoghue <john.donoghue@ieee.org>
2022-06-20 10:44:25 -0400

    * src/gdcm.mk: update to v3.0.13

--- src/gdcm.mk
+++ src/gdcm.mk
@@ -3,8 +3,8 @@
 
 PKG             := gdcm
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.0.12
-$(PKG)_CHECKSUM := 31b7d961201c5e7d0283611307f918ee4a16d024
+$(PKG)_VERSION  := 3.0.13
+$(PKG)_CHECKSUM := 9077d1b8539802254c5dd96176672820e0d20a5a
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG) 3.x/GDCM $($(PKG)_VERSION)/$($(PKG)_FILE)
