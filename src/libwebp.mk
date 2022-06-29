5d64f3643b80b4575fd42d3123a61f697ba5349b
John Donoghue <john.donoghue@ieee.org>
2022-06-29 11:04:08 -0400

    * src/libwebp.mk: update update rule

--- src/libwebp.mk
+++ src/libwebp.mk
@@ -12,7 +12,7 @@ $(PKG)_DEPS     :=
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://developers.google.com/speed/webp/download' | \
-    $(SED) -n 's,.*<a href="//storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-\([0-9][^"]*\)\.tar.gz">Download</a> |,\1,p' | \
+    $(SED) -n 's,.*<a href="/static//storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-\([0-9][^"]*\)\.tar.gz">Download</a> |,\1,p' | \
     $(SORT) -V | \
     tail -1
 endef
