4783c0d8669b070384fd582a69573b70d9de05fb
John Donoghue <john.donoghue@ieee.org>
2022-07-07 08:31:35 -0400

    * src/mesa.mk: update to v22.1.3

--- src/mesa.mk
+++ src/mesa.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := mesa
-$(PKG)_VERSION  := 22.1.2
-$(PKG)_CHECKSUM := c651193e4674d2afc2fe7b8f67e798ca63cfc2f7
+$(PKG)_VERSION  := 22.1.3
+$(PKG)_CHECKSUM := 8d27ed72beee3939b3e81793dc081c4a6dc69593
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.freedesktop.org/pub/mesa/$($(PKG)_FILE)
