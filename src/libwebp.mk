53221dedb7633f33d643e2b0c5a06acdb43ab0b3
John Donoghue <john.donoghue@ieee.org>
2022-07-22 08:07:40 -0400

    * src/libwebp.mk: update v1.2.3

--- src/libwebp.mk
+++ src/libwebp.mk
@@ -3,8 +3,8 @@
 
 PKG             := libwebp
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.2
-$(PKG)_CHECKSUM := 4e5e570b1ece450ec38233c52adbae253f81127e
+$(PKG)_VERSION  := 1.2.3
+$(PKG)_CHECKSUM := 6a7a8c41b02b2a98dbf2f639c74d9e187bb78ba0
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://storage.googleapis.com/downloads.webmproject.org/releases/webp/$($(PKG)_FILE)
