6f66a47a0ea48cdfe091b72c4f38ddfffe7115b3
John Donoghue <john.donoghue@ieee.org>
2022-08-25 07:45:31 -0400

    * src/sdl2_net.mk: update to v2.2.0

--- src/sdl2_net.mk
+++ src/sdl2_net.mk
@@ -3,8 +3,8 @@
 
 PKG             := sdl2_net
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.0.1
-$(PKG)_CHECKSUM := 99c17ee2ecf32c8a136163bbaa8f018f7a3e31e9
+$(PKG)_VERSION  := 2.2.0
+$(PKG)_CHECKSUM := 9154bd0898399dbe98c4525b0f3fcaf5b15c64fc
 $(PKG)_SUBDIR   := SDL2_net-$($(PKG)_VERSION)
 $(PKG)_FILE     := SDL2_net-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://www.libsdl.org/projects/SDL_net/release/$($(PKG)_FILE)
