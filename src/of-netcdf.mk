916013ce36b3fea56c520594b9e26b2a26569836
John Donoghue <john.donoghue@ieee.org>
2022-09-16 08:35:38 -0400

    of-netcdf: update to v1.0.16
    * src/of-netcdf.mk: update version, checksum
    
    * src/of-netcdf-1.patch: removed file
    (grafted from c3a99fe40c7a7095cd3efa18eb2a0ecbb5640ea9)

--- src/of-netcdf.mk
+++ src/of-netcdf.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-netcdf
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.15
-$(PKG)_CHECKSUM := 8692461e8303cd7e7648920bd0e385e81492efe3
+$(PKG)_VERSION  := 1.0.16
+$(PKG)_CHECKSUM := 7eece84184f4cea73d0aa57dca7f95e20f808e41
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := netcdf-$($(PKG)_VERSION)
 $(PKG)_FILE     := netcdf-$($(PKG)_VERSION).tar.gz
