d0eadafb76fc7d817c055e60be623aa6f6ef6a2a
John Donoghue <john.donoghue@ieee.org>
2022-10-16 17:42:20 -0400

    * src/zlib.mk: update to v1.2.13

--- src/zlib.mk
+++ src/zlib.mk
@@ -3,8 +3,8 @@
 
 PKG             := zlib
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.2.12
-$(PKG)_CHECKSUM := 37d26cd1138597c866b8e3e137156c82a0f0f960
+$(PKG)_VERSION  := 1.2.13
+$(PKG)_CHECKSUM := b459b42aa533968ae403c0002a255d62d3136954
 $(PKG)_SUBDIR   := zlib-$($(PKG)_VERSION)
 $(PKG)_FILE     := zlib-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://zlib.net/$($(PKG)_FILE)
