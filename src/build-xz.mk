aaccb7bde72243a75da65f5553067cf48abce905
John Donoghue <john.donoghue@ieee.org>
2022-10-16 17:42:19 -0400

    * src/build-xz.mk, src/xz.mk: update to v5.2.7

--- src/build-xz.mk
+++ src/build-xz.mk
@@ -3,8 +3,8 @@
 
 PKG             := build-xz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.2.6
-$(PKG)_CHECKSUM := cd2db279df5716d18e961695921d7f6de629c6e9
+$(PKG)_VERSION  := 5.2.7
+$(PKG)_CHECKSUM := 9a15569a7a969c03ae621c7ecbb29c187f59355e
 $(PKG)_SUBDIR   := xz-$($(PKG)_VERSION)
 $(PKG)_FILE     := xz-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://tukaani.org/xz/$($(PKG)_FILE)
