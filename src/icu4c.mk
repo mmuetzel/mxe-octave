df444fff1c0253644d06ac2df9f269d9fc221a95
John Donoghue <john.donoghue@ieee.org>
2022-10-19 16:57:24 -0400

    * src/icu4c.mk: update to v72.1

--- src/icu4c.mk
+++ src/icu4c.mk
@@ -1,7 +1,7 @@
 PKG             := icu4c
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 71.1
-$(PKG)_CHECKSUM := 406b0c8635288b772913b6ff646451e69748878a
+$(PKG)_VERSION  := 72.1
+$(PKG)_CHECKSUM := a97546f0119c37a3526143bc29fb573a4417ff84
 $(PKG)_SUBDIR   := icu
 $(PKG)_FILE     := $(PKG)-$(subst .,_,$($(PKG)_VERSION))-src.tgz
 $(PKG)_URL      := https://github.com/unicode-org/icu/releases/download/release-$(subst .,-,$($(PKG)_VERSION))/$($(PKG)_FILE)
