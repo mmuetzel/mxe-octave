7a456233e4a7f6b0f778a0c2929dfec9c8f132ee
Markus Mützel <markus.muetzel@gmx.de>
2022-11-16 08:25:43 +0100

    octave: Update version numbers after merge in Octave repository.
    * src/default-octave.mk: Update version number to 9.0.0.
    * src/stable-octave.mk: Update version number to 8.0.1. Merge changes in build
    rule from default-octave.mk.

--- src/stable-octave.mk
+++ src/stable-octave.mk
@@ -13,7 +13,7 @@
 
 PKG             := stable-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 7.3.1
+$(PKG)_VERSION  := 8.0.1
 $(PKG)_CHECKSUM := ## No checksum
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.lz
@@ -112,9 +112,9 @@ else
 endif
 
 ifeq ($(ENABLE_FORTRAN_INT64),yes)
-  $(PKG)_ENABLE_FORTRAN_INT64_CONFIGURE_OPTIONS := F77_INTEGER_8_FLAG=-fdefault-integer-8 ax_blas_f77_func_ok=yes ax_blas_integer_size=8 octave_cv_sizeof_fortran_integer=8
+  $(PKG)_ENABLE_FORTRAN_INT64_CONFIGURE_OPTIONS := F77_INTEGER_8_FLAG=-fdefault-integer-8 ax_blas_f77_func_ok=yes ax_cv_blas_integer_size=8 octave_cv_sizeof_fortran_integer=8
 else
-  $(PKG)_ENABLE_FORTRAN_INT64_CONFIGURE_OPTIONS := ax_blas_f77_func_ok=yes ax_blas_integer_size=4 octave_cv_sizeof_fortran_integer=4
+  $(PKG)_ENABLE_FORTRAN_INT64_CONFIGURE_OPTIONS := ax_blas_f77_func_ok=yes ax_cv_blas_integer_size=4 octave_cv_sizeof_fortran_integer=4
 endif
 
 ifeq ($(MXE_SYSTEM),mingw)
