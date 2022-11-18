e2afd3c1cdc31bc990dc2e7b41812b7afbfb13ad
John Donoghue <john.donoghue@ieee.org>
2022-11-18 13:18:35 -0500

    * src/cfitsio.mk: update to v4.2.0

--- src/cfitsio.mk
+++ src/cfitsio.mk
@@ -3,8 +3,8 @@
 
 PKG             := cfitsio
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.1.0
-$(PKG)_CHECKSUM := 3c63ceccc4de3495b22d5a92e849d2ce027d07a6
+$(PKG)_VERSION  := 4.2.0
+$(PKG)_CHECKSUM := 165f0af3c9931ae5ad0dc3eb396a10711b4fdd6b
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/$($(PKG)_FILE)
