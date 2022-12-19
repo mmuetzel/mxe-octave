c5af2083bf280f11237c6097e732985d8aa16382
Markus Mützel <markus.muetzel@gmx.de>
2022-12-19 16:10:21 +0100

    of-statistics: Update checksum.
    * src/of-statistics.mk: It looks like the tarball of version 1.5.2 has been
    exchanged without bumping its version number. Update checksum to work-around
    that upstream issue.

--- src/of-statistics.mk
+++ src/of-statistics.mk
@@ -4,7 +4,7 @@
 PKG             := of-statistics
 $(PKG)_IGNORE   :=
 $(PKG)_VERSION  := 1.5.2
-$(PKG)_CHECKSUM := c141b257895dd4799430b1852b836ed4cb6aabac
+$(PKG)_CHECKSUM := f4f27430c79140f7c13b3c230c92a1ba6802f0c5
 $(PKG)_REMOTE_SUBDIR :=
 $(PKG)_SUBDIR   := statistics-release-$($(PKG)_VERSION)
 $(PKG)_FILE     := statistics-$($(PKG)_VERSION).tar.gz
