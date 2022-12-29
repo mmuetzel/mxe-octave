ca804fa199c676bcb0f2cf7ef203f673b75481c0
Markus Mützel <markus.muetzel@gmx.de>
2022-12-29 13:03:34 +0100

    * src/tiff.mk: Avoid picking up deflate library from build system.

--- src/tiff.mk
+++ src/tiff.mk
@@ -26,7 +26,9 @@ define $(PKG)_BUILD
         $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
         $(ENABLE_SHARED_OR_STATIC) \
         --prefix='$(HOST_PREFIX)' \
-        --enable-jbig --without-x && $(CONFIGURE_POST_HOOK)
+        --enable-jbig \
+        --disable-libdeflate \
+        --without-x && $(CONFIGURE_POST_HOOK)
     $(MAKE) -C '$(1)' -j '$(JOBS)' $(MXE_DISABLE_PROGS) DESTDIR='$(3)'
     $(MAKE) -C '$(1)' -j 1 install $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) DESTDIR='$(3)'
 
