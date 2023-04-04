13eb998f2365108bb01c07eee0a8818e001102e4
John Donoghue <john.donoghue@ieee.org>
2023-04-03 20:41:30 -0400

    * src/openblas.mk: update to v0.3.23

--- src/openblas.mk
+++ src/openblas.mk
@@ -3,8 +3,8 @@
 
 PKG             := openblas
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.3.21
-$(PKG)_CHECKSUM := b052d196ad694b29302e074b3eb8cc66745f6e2f
+$(PKG)_VERSION  := 0.3.23
+$(PKG)_CHECKSUM := 6b781727c7b95850ae4a3eb0a391492eb4f3e780
 $(PKG)_SUBDIR   := OpenBLAS-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/xianyi/OpenBLAS/archive/v$($(PKG)_VERSION).tar.gz
