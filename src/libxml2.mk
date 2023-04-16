8d659eeb4bd87fd661ea5ea8e591df43db85553d
John Donoghue <john.donoghue@ieee.org>
2023-04-16 07:59:17 -0400

    * src/libxml2.mk: update to v2.10.4

--- src/libxml2.mk
+++ src/libxml2.mk
@@ -3,8 +3,8 @@
 
 PKG             := libxml2
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.10.3
-$(PKG)_CHECKSUM := 18aa1ab0caf4ca8b092613acac992a33fa432311
+$(PKG)_VERSION  := 2.10.4
+$(PKG)_CHECKSUM := b57d52b9edcad3271de112c635069f9b35372888
 $(PKG)_SUBDIR   := libxml2-v$($(PKG)_VERSION)
 $(PKG)_FILE     := libxml2-v$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://gitlab.gnome.org/GNOME/$(PKG)/-/archive/v$($(PKG)_VERSION)/$($(PKG)_FILE)
