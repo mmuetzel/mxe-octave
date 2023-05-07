4a23a100c8a3d3be0366c8eb75346f74933431ab
John Donoghue <john.donoghue@ieee.org>
2023-05-07 15:51:52 -0400

    * src/of-splines.mk: update to v1.3.5

--- src/of-splines.mk
+++ src/of-splines.mk
@@ -3,10 +3,10 @@
 
 PKG             := of-splines
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.3.4
-$(PKG)_CHECKSUM := 47295983e9fc0a6c9fd91167f404c1d1b4f7cb6a
+$(PKG)_VERSION  := 1.3.5
+$(PKG)_CHECKSUM := 9e478de38862dc97334cd723f3cfb4611712b47d
 $(PKG)_REMOTE_SUBDIR := 
-$(PKG)_SUBDIR   := splines-$($(PKG)_VERSION)
+$(PKG)_SUBDIR   := splines
 $(PKG)_FILE     := splines-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
 $(PKG)_DEPS     := 
