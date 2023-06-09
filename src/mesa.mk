c21602946404e7040db09a9e0f680f6b2b77fb56
John Donoghue <john.donoghue@ieee.org>
2023-06-09 08:44:04 -0400

    * src/mesa.mk: update to v23.1.2

--- src/mesa.mk
+++ src/mesa.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := mesa
-$(PKG)_VERSION  := 23.1.1
-$(PKG)_CHECKSUM := 30ecffcddb4e8fec2b208973c7e06d566afa1060
+$(PKG)_VERSION  := 23.1.2
+$(PKG)_CHECKSUM := 772a8d24ef3af300e173cff754c78e6d04fc163a
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.freedesktop.org/pub/mesa/$($(PKG)_FILE)
