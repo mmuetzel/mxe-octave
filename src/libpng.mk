96c15e0878daa735e32ba06e41cab7e4be0e7d5c
John Donoghue <john.donoghue@ieee.org>
2023-06-22 11:53:47 -0400

    * src/libpng.mk: update to v1.6.40

--- src/libpng.mk
+++ src/libpng.mk
@@ -3,8 +3,8 @@
 
 PKG             := libpng
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.6.39
-$(PKG)_CHECKSUM := 3f2386d61eccae211ec4f57899e4ac2ca60d390b
+$(PKG)_VERSION  := 1.6.40
+$(PKG)_CHECKSUM := 1c37609e3f0740ae52ca9e2a6adfc9743497b870
 $(PKG)_SUBDIR   := libpng-$($(PKG)_VERSION)
 $(PKG)_FILE     := libpng-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)$(subst .,,$(call SHORT_PKG_VERSION,$(PKG)))/$($(PKG)_VERSION)/$($(PKG)_FILE)
