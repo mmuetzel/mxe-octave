c5884f9b3ec9100d696a37934935e94e2de05838
John Donoghue <john.donoghue@ieee.org>
2023-07-25 10:34:34 -0400

    * src/opkg-mqtt.mk: update to v0.0.4 (grafted from 96d7401c2e8930cd3ed6d1dd2cab54ba27dfa877)

--- src/opkg-mqtt.mk
+++ src/opkg-mqtt.mk
@@ -3,11 +3,11 @@
 
 PKG             := opkg-mqtt
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.0.3
-$(PKG)_CHECKSUM := b5d4745e7ab56f500ae00c17788843c024475847
+$(PKG)_VERSION  := 0.0.4
+$(PKG)_CHECKSUM := 01d9b12a4b90be0286c48099e9dc7b4fa1b127ff
 $(PKG)_SUBDIR   := octave-mqtt-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
-$(PKG)_URL      := https://downloads.sourceforge.net/project/octave-mqtt/v$($(PKG)_VERSION)/$($(PKG)_FILE)
+$(PKG)_URL      := https://github.com/gnu-octave/octave-mqtt/releases/download/release-$($(PKG)_VERSION)/$($(PKG)_SUBDIR).tar.gz
 $(PKG)_DEPS     := paho-mqtt-c
 
 ifeq ($(ENABLE_BINARY_PACKAGES),yes)
@@ -15,9 +15,7 @@ ifeq ($(ENABLE_BINARY_PACKAGES),yes)
 endif
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- https://sourceforge.net/p/octave-mqtt/code/ci/default/tree/ | \
-    $(SED) -n 's|.*code/ci/release-\([0-9][0-9.]*\)/tree.*|\1|p' | $(SORT) -V | \
-    tail -1
+    $(call GITHUB_PKG_UPDATE,gnu-octave,octave-mqtt,release-)
 endef
 
 define $(PKG)_BUILD
