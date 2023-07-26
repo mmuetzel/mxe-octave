c69f7fbf4d6b82d88e55dfc03d7a64fa7776b67a
John Donoghue <john.donoghue@ieee.org>
2023-07-26 11:39:08 -0400

    * src/sundials-ida.mk: update to v6.6.0

--- src/sundials-ida.mk
+++ src/sundials-ida.mk
@@ -3,8 +3,8 @@
 
 PKG             := sundials-ida
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.5.1
-$(PKG)_CHECKSUM := 8efac29bb988c08f41ea8f06534d925922fc3b73
+$(PKG)_VERSION  := 6.6.0
+$(PKG)_CHECKSUM := 2d77068f5704c692ab8990b7abf064cc3bc6e987
 $(PKG)_SUBDIR   := sundials-$($(PKG)_VERSION)
 $(PKG)_FILE     := sundials-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/LLNL/sundials/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
