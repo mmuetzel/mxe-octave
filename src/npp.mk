a7d5ae6bd02436ffe12d03a41bbdb12ce7f5099c
John Donoghue <john.donoghue@ieee.org>
2023-08-15 19:54:37 -0400

    * src/npp.mk: update to v8.5.6

--- src/npp.mk
+++ src/npp.mk
@@ -3,8 +3,8 @@
 
 PKG             := npp
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 8.5.5
-$(PKG)_CHECKSUM := a97f88d3adb056ffc25f0552829d338ca46156fb
+$(PKG)_VERSION  := 8.5.6
+$(PKG)_CHECKSUM := a2094e6356688a5b416a5824f17b628ab35b7222
 $(PKG)_SUBDIR   := 
 $(PKG)_FILE     := $(PKG).$($(PKG)_VERSION).portable.zip
 $(PKG)_URL      := https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
