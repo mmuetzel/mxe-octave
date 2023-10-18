b3fb85632a638a6a05fa889c59a5eda892284b1a
John Donoghue <john.donoghue@ieee.org>
2023-10-18 07:17:41 -0400

    netcdf: update to v4.9.2
    * src/netcdf.mk: update to 4.9.2, use cmake
    
    * src/netcdf-1-fixes.patch: removed file
    
    * dist-files.mk: removed ref to patch file

--- src/netcdf.mk
+++ src/netcdf.mk
@@ -3,8 +3,8 @@
 
 PKG             := netcdf
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.8.1
-$(PKG)_CHECKSUM := ae9e8741aebdb3ba4494a78a7f5a32e92516edc1
+$(PKG)_VERSION  := 4.9.2
+$(PKG)_CHECKSUM := 0eb6fdcbd725bd53bb9e98405a2cb15f3c511ce2
 $(PKG)_SUBDIR   := netcdf-c-$($(PKG)_VERSION)
 $(PKG)_FILE     := netcdf-c-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/Unidata/netcdf-c/archive/v$($(PKG)_VERSION).tar.gz
@@ -14,32 +14,27 @@ define $(PKG)_UPDATE
     $(call GITHUB_PKG_UPDATE,Unidata,netcdf-c,v)
 endef
 
-ifeq ($(MXE_WINDOWS_BUILD),yes)
-  $(PKG)_CONFIGURE_OPTIONS := CPPFLAGS='-DH5_USE_110_API -DDLL_NETCDF -I$(HOST_PREFIX)/include'
-else
-  $(PKG)_CONFIGURE_OPTIONS := CPPFLAGS='-DH5_USE_110_API -I$(HOST_PREFIX)/include'
-endif
-
-$(PKG)_CONFIGURE_POST_HOOK := $(CONFIGURE_POST_HOOK)
-ifeq ($(MXE_SYSTEM),msvc)
-    $(PKG)_CONFIGURE_POST_HOOK += -x
-endif
-
 define $(PKG)_BUILD
-    if [ $(MXE_SYSTEM) = msvc ]; then \
-        cd '$(1)' && autoreconf -f -i -v; \
-    fi
-    cd '$(1)' && ./configure \
-        $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
-	--enable-hdf5 --disable-testsets --disable-utilities \
-        $(CONFIGURE_CPPFLAGS) $(CONFIGURE_LDFLAGS) \
-        $(ENABLE_SHARED_OR_STATIC) \
-        --prefix='$(HOST_PREFIX)' \
-        $($(PKG)_CONFIGURE_OPTIONS) \
-	&& $($(PKG)_CONFIGURE_POST_HOOK)
-    $(MAKE) -C '$(1)' -j '$(JOBS)' LDFLAGS='-no-undefined -L$(HOST_LIBDIR)' $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) V=1
-    $(MAKE) -C '$(1)' -j 1 install LDFLAGS='-no-undefined -L$(HOST_LIBDIR)' $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) DESTDIR='$(3)'
-  
+    cd '$(1)' && cmake \
+        $($(PKG)_CMAKE_FLAGS) \
+        -DBUILD_UTILITIES=OFF \
+        -DENABLE_EXAMPLES=OFF \
+        -DENABLE_TESTS=OFF \
+        -DENABLE_PARALLEL_TESTS=OFF \
+        -DENABLE_DAP_REMOTE_TESTS=OFF \
+        -DENABLE_DAP=ON \
+        -DENABLE_NETCDF_4=ON \
+        -DENABLE_BYTERANGE=OFF \
+        -DENABLE_LOGGING=ON \
+        -DBUILD_TESTING=no \
+        $(CMAKE_CCACHE_FLAGS) \
+        $(CMAKE_BUILD_SHARED_OR_STATIC) \
+        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        .
+
+    $(MAKE) -C '$(1)' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(1)' -j '1' VERBOSE=1 DESTDIR='$(3)' install
+
     if [ ! "x$(MXE_NATIVE_BUILD)" = "xyes" ]; then \
       $(LN_SF) '$(HOST_BINDIR)/nc-config' '$(BUILD_TOOLS_PREFIX)/bin/$(MXE_TOOL_PREFIX)nc-config'; \
     fi
