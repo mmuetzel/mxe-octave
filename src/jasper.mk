6ec0532717baeff69ff79d9fdb2b6b735fc72bd0
John Donoghue <john.donoghue@ieee.org>
2023-12-18 10:31:07 -0500

    jasper: update to v4.1.1
    * src/jasper-1-fixes.patch: update patch
    
    * src/jasper.mk: update to v4.1.1, use cmake, get source from github

--- src/jasper.mk
+++ src/jasper.mk
@@ -3,29 +3,38 @@
 
 PKG             := jasper
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.900.1
-$(PKG)_CHECKSUM := 9c5735f773922e580bf98c7c7dfda9bbed4c5191
-$(PKG)_SUBDIR   := jasper-$($(PKG)_VERSION)
-$(PKG)_FILE     := jasper-$($(PKG)_VERSION).zip
-$(PKG)_URL      := http://www.ece.uvic.ca/~mdadams/jasper/software/$($(PKG)_FILE)
+$(PKG)_VERSION  := 4.1.1
+$(PKG)_CHECKSUM := 25f0d383c5360f7b7421a43f022e13aa0141c956
+$(PKG)_SUBDIR   := jasper-version-$($(PKG)_VERSION)
+$(PKG)_FILE     := jasper-version-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := https://github.com/jasper-software/$(PKG)/archive/version-$($(PKG)_VERSION).tar.gz
 $(PKG)_DEPS     := jpeg
 
+
+ifeq ($(MXE_NATIVE_BUILD),no)
+$(PKG)_CMAKE_FLAGS = -DJAS_STDC_VERSION="`'$(MXE_CC)' -dM -E - < /dev/null | grep __STDC_VERSION__ | '$(SED)' 's/^\([^ ]\+ \)\{2\}//;'`"
+else
+$(PKG)_CMAKE_FLAGS :=
+endif
+
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://www.ece.uvic.ca/~mdadams/jasper/' | \
-    grep 'jasper-' | \
-    $(SED) -n 's,.*jasper-\([0-9][^>]*\)\.zip.*,\1,p' | \
-    head -1
+    $(call GITHUB_PKG_UPDATE,jasper-software,jasper,version-)
 endef
 
 define $(PKG)_BUILD
-    cd '$(1)' && autoreconf -fi -I m4 && ./configure \
-        $(HOST_AND_BUILD_CONFIGURE_OPTIONS) \
-        $(CONFIGURE_CPPFLAGS) \
-        $(CONFIGURE_LDFLAGS) \
-        $(ENABLE_SHARED_OR_STATIC) \
-        --prefix='$(HOST_PREFIX)' \
-        --enable-libjpeg \
-        --disable-opengl \
-        --without-x && $(CONFIGURE_POST_HOOK)
-    $(MAKE) -C '$(1)' -j '$(JOBS)' install $(MXE_DISABLE_PROGS) $(MXE_DISABLE_DOCS) DESTDIR='$(3)'
+    mkdir $(1).build && cd '$(1).build' && cmake \
+        $($(PKG)_CMAKE_FLAGS) \
+        -DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=OFF \
+        -DJAS_ENABLE_LIBJPEG=ON \
+        -DJAS_ENABLE_OPENGL=OFF \
+        -DJAS_ENABLE_PROGRAMS=OFF \
+        -DJAS_ENABLE_DOC=OFF \
+        -DJAS_ENABLE_SHARED=$(if $(findstring yes,$(BUILD_SHARED)),ON,OFF) \
+        -DBUILD_TESTING=NO \
+        $(CMAKE_CCACHE_FLAGS) \
+        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        $(1)
+
+    $(MAKE) -C '$(1).build' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(1).build' -j '1' VERBOSE=1 DESTDIR='$(3)' install
 endef
