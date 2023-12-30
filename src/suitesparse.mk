ae6d139225d392ec5bd87f4c63a9531f7c41e9db
Markus Mützel <markus.muetzel@gmx.de>
2023-12-30 19:06:45 +0100

    SuiteSparse: update to 7.4.0
    * src/suitesparse.mk: Update version and checksum. Use new CMake based build
    system.
    
    * src/glpk-1-fixes.patch: Use pkg-config to get location of SuiteSparse headers.

--- src/suitesparse.mk
+++ src/suitesparse.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := suitesparse
-$(PKG)_VERSION  := 7.3.1
-$(PKG)_CHECKSUM := c06cd64f052d2187f8945348f561105ced494317
+$(PKG)_VERSION  := 7.4.0
+$(PKG)_CHECKSUM := c2709fc33b843d4830e6cedc4d4821d49ac99bc0
 $(PKG)_SUBDIR   := SuiteSparse-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v$($(PKG)_VERSION).tar.gz
@@ -24,11 +24,7 @@ endef
 
 
 ifeq ($(ENABLE_FORTRAN_INT64),yes)
-  $(PKG)_CMAKE_FLAGS += -DALLOW_64BIT_BLAS=ON
-endif
-
-ifneq ($(BUILD_STATIC),yes)
-  $(PKG)_CMAKE_FLAGS += -DNSTATIC=ON
+  $(PKG)_CMAKE_FLAGS += -DSUITESPARSE_USE_64BIT_BLAS=ON
 endif
 
 $(PKG)_MAKE_OPTS = \
@@ -36,21 +32,22 @@ $(PKG)_MAKE_OPTS = \
     CFLAGS='$(MXE_CFLAGS)' \
     CXXFLAGS='$(MXE_CXXFLAGS)' \
     AR='$(MXE_AR)' \
-    RANLIB='$(MXE_RANLIB)' \
-    CMAKE_OPTIONS='-DCMAKE_TOOLCHAIN_FILE="$(CMAKE_TOOLCHAIN_FILE)" \
-                   -DCOMPACT=ON -DNOPENMP=ON \
-                   -DBLA_VENDOR="Generic" -DBLAS_LIBRARIES="-lblas -lgfortran" -DLAPACK_LIBRARIES="-llapack" \
-                   -DENABLE_CUDA=OFF \
-                   $($(PKG)_CMAKE_FLAGS) \
-                   $(CMAKE_CCACHE_FLAGS) $(CMAKE_BUILD_SHARED_OR_STATIC)'
+    RANLIB='$(MXE_RANLIB)'
 
 define $(PKG)_BUILD
-    # build all
-    $(MAKE) -C '$(1)' -j '$(JOBS)' \
-        $($(PKG)_MAKE_OPTS)
+    cd '$(1)/build' && $($(PKG)_MAKE_OPTS) cmake \
+        $($(PKG)_CMAKE_FLAGS) \
+        $(CMAKE_CCACHE_FLAGS) \
+        -DCMAKE_TOOLCHAIN_FILE='$(CMAKE_TOOLCHAIN_FILE)' \
+        -DSUITESPARSE_CROSS_TOOLCHAIN_FLAGS_NATIVE="-DCMAKE_TOOLCHAIN_FILE=$(CMAKE_NATIVE_TOOLCHAIN_FILE)" \
+        -DGRAPHBLAS_COMPACT=ON \
+        -DSUITESPARSE_USE_OPENMP=OFF \
+        -DBLA_VENDOR="Generic" -DBLAS_LIBRARIES="-lblas -lgfortran" -DLAPACK_LIBRARIES="-llapack" \
+        -DSUITESPARSE_USE_CUDA=OFF \
+        $(CMAKE_CCACHE_FLAGS) $(CMAKE_BUILD_SHARED_OR_STATIC) \
+        $(1)
 
-    # install libraries and headers
-    $(MAKE) -C '$(1)' -j 1 install \
-        $($(PKG)_MAKE_OPTS)
+    cmake --build '$(1)/build'
+    DESTDIR='$(3)' cmake --install '$(1)/build'
 endef
 
