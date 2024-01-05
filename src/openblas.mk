b110d338d55d8652e209cce03487ce404c585089
John Donoghue <john.donoghue@ieee.org>
2024-01-05 16:50:58 -0500

    * src/openblas.mk: update to v0.3.26

--- src/openblas.mk
+++ src/openblas.mk
@@ -3,8 +3,8 @@
 
 PKG             := openblas
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.3.25
-$(PKG)_CHECKSUM := 79f2828dff06e24ecec7277f3d3da5e7b196ff53
+$(PKG)_VERSION  := 0.3.26
+$(PKG)_CHECKSUM := af6dce0e486e684be842053fcbd98eaf97e39749
 $(PKG)_SUBDIR   := OpenBLAS-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/xianyi/OpenBLAS/archive/v$($(PKG)_VERSION).tar.gz
