18dad2c3da4f1869886648d6ab738944869344e6
John Donoghue <john.donoghue@ieee.org>
2024-02-09 07:56:02 -0500

    * src/sdl2.mk: update to v2.30.0

--- src/sdl2.mk
+++ src/sdl2.mk
@@ -3,8 +3,8 @@
 
 PKG             := sdl2
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.28.5
-$(PKG)_CHECKSUM := 50af6b564890d702e57e2a72d6429b43778ed29a
+$(PKG)_VERSION  := 2.30.0
+$(PKG)_CHECKSUM := 02c5bc582aa22715c3fe88364518cfd29e13ecd3
 $(PKG)_SUBDIR   := SDL2-$($(PKG)_VERSION)
 $(PKG)_FILE     := SDL2-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.libsdl.org/release/$($(PKG)_FILE)
