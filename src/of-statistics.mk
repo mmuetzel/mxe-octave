0b6e6e6b7985adc3f957ac7c9a469a88846b014f
John Donoghue <john.donoghue@ieee.org>
2024-02-09 07:55:56 -0500

    * src/of-statistics.mk: update to v1.6.2

--- src/of-statistics.mk
+++ src/of-statistics.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-statistics
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.6.1
-$(PKG)_CHECKSUM := caaa1f80f11d7a591af15d55ac741e1b3c6ce40b
+$(PKG)_VERSION  := 1.6.2
+$(PKG)_CHECKSUM := fef85a8bab1b355a6fce05e243ae370f50affa07
 $(PKG)_REMOTE_SUBDIR :=
 $(PKG)_SUBDIR   := statistics-release-$($(PKG)_VERSION)
 $(PKG)_FILE     := statistics-$($(PKG)_VERSION).tar.gz
