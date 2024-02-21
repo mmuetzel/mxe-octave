f6e2061c4e65ca3d9d7473bee7457a74a3d075f7
John Donoghue <john.donoghue@ieee.org>
2024-02-21 18:52:06 -0500

    * src/jasper.mk: update to v4.2.1

--- src/jasper.mk
+++ src/jasper.mk
@@ -3,8 +3,8 @@
 
 PKG             := jasper
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.2.0
-$(PKG)_CHECKSUM := f799047228b629eccc9e59de6aa9ae8c649c2326
+$(PKG)_VERSION  := 4.2.1
+$(PKG)_CHECKSUM := 42949b6da8060e02b1d07b15e44fca367ed99c8e
 $(PKG)_SUBDIR   := jasper-version-$($(PKG)_VERSION)
 $(PKG)_FILE     := jasper-version-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/jasper-software/$(PKG)/archive/version-$($(PKG)_VERSION).tar.gz
