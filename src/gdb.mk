fa9b65bfe83b2523ea232286cbc26da95f40087e
John Donoghue <john.donoghue@ieee.org>
2024-03-03 14:37:42 -0500

    * src/gdb.mk: update to v14.2

--- src/gdb.mk
+++ src/gdb.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := gdb
-$(PKG)_VERSION  := 14.1
-$(PKG)_CHECKSUM := d0b703c4140e0a66f6c80f2f9c4a2e8da7dcac25
+$(PKG)_VERSION  := 14.2
+$(PKG)_CHECKSUM := 4f38f7c24d523b6923f22404b7dee4152a00d0d4
 $(PKG)_SUBDIR   := gdb-$($(PKG)_VERSION)
 $(PKG)_FILE     := gdb-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/$(PKG)/$($(PKG)_FILE)
