3b0f40865e1baf4aed58ea24ece50e444f3e996a
John Donoghue <john.donoghue@ieee.org>
2024-04-01 07:53:53 -0400

    * src/nsis.mk: update to 7405-2

--- src/nsis.mk
+++ src/nsis.mk
@@ -3,11 +3,11 @@
 
 PKG             := nsis
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 7405-1
-$(PKG)_CHECKSUM := 676a8ec96642e36b5e091b28f08ace524f8202b3
+$(PKG)_VERSION  := 7405-2
+$(PKG)_CHECKSUM := 3e08f43c1f19a136119398b3ee04747581f92750
 $(PKG)_SUBDIR   := nsis-code-$($(PKG)_VERSION)-NSIS-trunk
 $(PKG)_FILE     := nsis-code-$($(PKG)_VERSION)-NSIS-trunk.zip
-$(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/nsisbi/nsisbi3.09.1/$($(PKG)_FILE)
+$(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/nsisbi/nsisbi3.09.2/$($(PKG)_FILE)
 $(PKG)_DEPS     := build-scons build-setuptools
 
 define $(PKG)_UPDATE
