bfb8382bafd95de4e60e40d318a3ca354dd02bac
John Donoghue <john.donoghue@ieee.org>
2024-06-22 08:01:58 -0400

    * src/llvm.mk: update to v18.1.8

--- src/llvm.mk
+++ src/llvm.mk
@@ -3,8 +3,8 @@
 
 PKG             := llvm
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 18.1.7
-$(PKG)_CHECKSUM := 824957de63cb2edf525a0e6f1fa290ea0e2791bd
+$(PKG)_VERSION  := 18.1.8
+$(PKG)_CHECKSUM := f9befa4cbef3f688ab48fca42449e13c5bcb872d
 $(PKG)_SUBDIR   := llvm-$($(PKG)_VERSION).src
 $(PKG)_FILE     := llvm-$($(PKG)_VERSION).src.tar.xz
 $(PKG)_URL      := https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(PKG)_VERSION)/$($(PKG)_FILE)
