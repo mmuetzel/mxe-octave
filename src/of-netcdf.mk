529b1b401e1e719c7dd4221ec688dfc7b1b6ae43
John Donoghue <john.donoghue@ieee.org>
2024-09-24 13:13:21 -0400

    * src/of-netcdf.mk: update to v1.0.18

--- src/of-netcdf.mk
+++ src/of-netcdf.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-netcdf
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.17
-$(PKG)_CHECKSUM := 49178ff4b2b8be57c5c31e412469720ad1c02aca
+$(PKG)_VERSION  := 1.0.18
+$(PKG)_CHECKSUM := ee871fd73f342a4070fc17f6c81227f503f3385e
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := netcdf-$($(PKG)_VERSION)
 $(PKG)_FILE     := netcdf-$($(PKG)_VERSION).tar.gz
