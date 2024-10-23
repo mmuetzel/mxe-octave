44d6487b65dc8a2c79aa275ca64ac688fe7ad4e4
John Donoghue <john.donoghue@ieee.org>
2024-10-23 07:42:34 -0400

    * src/opkg-tablicious.mk: update to v0.4.4 (grafted from 43221f4aa2bf87890183709c978d371225578dc1)

--- src/opkg-tablicious.mk
+++ src/opkg-tablicious.mk
@@ -3,8 +3,8 @@
 
 PKG             := opkg-tablicious
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.4.2
-$(PKG)_CHECKSUM := ae8900835787e691b2841474076cbd60ac4c4ff6
+$(PKG)_VERSION  := 0.4.4
+$(PKG)_CHECKSUM := ca6bc550a70cdc2246b62e37b44b12c2300e1979
 $(PKG)_SUBDIR   := tablicious-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/apjanke/octave-tablicious/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
