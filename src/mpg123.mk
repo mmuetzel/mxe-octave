548d46fd91cb85d47eb8b81cf8edfe9a6ac67d52
John Donoghue <john.donoghue@ieee.org>
2024-12-26 15:38:32 -0500

    * src/mpg123.mk: update to v1.32.10

--- src/mpg123.mk
+++ src/mpg123.mk
@@ -3,8 +3,8 @@
 
 PKG             := mpg123
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.32.9
-$(PKG)_CHECKSUM := 25aea7c5edfe6bc637f603c075210c13955200fe
+$(PKG)_VERSION  := 1.32.10
+$(PKG)_CHECKSUM := 6fc6b73016bd616326bf22426ba6a619443a6cdb
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/mpg123/$(PKG)/$($(PKG)_VERSION)/$($(PKG)_FILE)
