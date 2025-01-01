6eed752240ca23c5ebee93bca71b29fe5ac24023
John Donoghue <john.donoghue@ieee.org>
2025-01-01 14:45:46 -0500

    * src/pixman.mk: update to v0.44.2

--- src/pixman.mk
+++ src/pixman.mk
@@ -3,8 +3,8 @@
 
 PKG             := pixman
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 0.44.0
-$(PKG)_CHECKSUM := e8a04db7a76bdbe3668650822da524110e42467f
+$(PKG)_VERSION  := 0.44.2
+$(PKG)_CHECKSUM := fc8407e2af52d1581ab3e31c16ce927a1b3399a8
 $(PKG)_SUBDIR   := pixman-$($(PKG)_VERSION)
 $(PKG)_FILE     := pixman-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://cairographics.org/releases/$($(PKG)_FILE)
