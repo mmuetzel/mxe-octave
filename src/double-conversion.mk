caa20c325c3ac4b94b813cd0c9a52a05d441245d
John Donoghue <john.donoghue@ieee.org>
2025-03-06 10:23:32 -0500

    * src/double-conversion.mk: update to v3.3.1

--- src/double-conversion.mk
+++ src/double-conversion.mk
@@ -3,8 +3,8 @@
 
 PKG             := double-conversion
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.3.0
-$(PKG)_CHECKSUM := 31bbee2c38287a71901e818010dd9427567e6c1f
+$(PKG)_VERSION  := 3.3.1
+$(PKG)_CHECKSUM := e2b0360379d1c5e86c68efb2cf4f9aa2c69d715f
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/google/$(PKG)/archive/v$($(PKG)_VERSION).tar.gz
