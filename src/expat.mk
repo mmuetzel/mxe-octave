0e893d6b7ba3a92bf35ff23da158f23d74b751bb
John Donoghue <john.donoghue@ieee.org>
2025-03-20 20:02:42 -0400

    * src/expat.mk: update to v2.7.0

--- src/expat.mk
+++ src/expat.mk
@@ -3,8 +3,8 @@
 
 PKG             := expat
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.6.4
-$(PKG)_CHECKSUM := 93082742ac825a8d399ab8f94c41d9f246c6d8bc
+$(PKG)_VERSION  := 2.7.0
+$(PKG)_CHECKSUM := 8ef9c2c38adc48246da88dd3bd24c590b900c5bf
 $(PKG)_SUBDIR   := expat-$($(PKG)_VERSION)
 $(PKG)_FILE     := expat-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/expat/expat/$($(PKG)_VERSION)/$($(PKG)_FILE)
