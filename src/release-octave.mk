d1aae2c1bc0df9929f46faabd70bc4aba6e899f3
John W. Eaton <jwe@octave.org>
2025-05-29 09:04:40 -0400

    update version number for Octave 10.2.0 release
    * src/stable-octave.mk: Update version to 10.2.1.
    * src/release-octave.mk: Update version to 10.2.0.  Update checksum
    for release.

--- src/release-octave.mk
+++ src/release-octave.mk
@@ -34,8 +34,8 @@
 
 PKG             := release-octave
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 10.1.0
-$(PKG)_CHECKSUM := ecab2437e2cc8b5ac8bdf3dbf73fd15538295dc1
+$(PKG)_VERSION  := 10.2.0
+$(PKG)_CHECKSUM := 7105d799d222f24a709ff8785eee265b37832002
 $(PKG)_SUBDIR   := octave-$($(PKG)_VERSION)
 $(PKG)_FILE     := octave-$($(PKG)_VERSION).tar.lz
 $(PKG)_URL      := ftp://ftp.gnu.org/gnu/octave/$($(PKG)_FILE)
