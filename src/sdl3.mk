2dd6254efd9a848086e741ab24e4c2eaefb31a8c
John Donoghue <john.donoghue@ieee.org>
2025-06-28 09:30:06 -0400

    * src/sdl3.mk: update to v3.2.16

--- src/sdl3.mk
+++ src/sdl3.mk
@@ -3,8 +3,8 @@
 
 PKG             := sdl3
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2.14
-$(PKG)_CHECKSUM := 39714bc4c836eba06d447aa107db55d85815ce12
+$(PKG)_VERSION  := 3.2.16
+$(PKG)_CHECKSUM := c4e75bad691322511d8d6830b7aa9f763a3c4fba
 $(PKG)_SUBDIR   := SDL3-$($(PKG)_VERSION)
 $(PKG)_FILE     := SDL3-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.libsdl.org/release/$($(PKG)_FILE)
