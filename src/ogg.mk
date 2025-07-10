f4239fcb29119690b1a68be54c3b098cc8413258
John Donoghue <john.donoghue@ieee.org>
2025-07-10 18:59:17 -0400

    * src/ogg.mk: update to v1.3.6

--- src/ogg.mk
+++ src/ogg.mk
@@ -3,8 +3,8 @@
 
 PKG             := ogg
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.3.5
-$(PKG)_CHECKSUM := 56f24c585af430e6714f96d096c60ddf9539ebfd
+$(PKG)_VERSION  := 1.3.6
+$(PKG)_CHECKSUM := 8f50ef58f4d4ba56a0d274b6ba7661066b3d4505
 $(PKG)_SUBDIR   := libogg-$($(PKG)_VERSION)
 $(PKG)_FILE     := libogg-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://downloads.xiph.org/releases/ogg/$($(PKG)_FILE)
