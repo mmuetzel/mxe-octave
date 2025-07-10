dbcbbdbe25352f0a06924b6379d2272b03f56d41
John Donoghue <john.donoghue@ieee.org>
2025-07-10 19:46:27 -0400

    * src/build-m4.mk: update to v1.4.20

--- src/build-m4.mk
+++ src/build-m4.mk
@@ -3,15 +3,15 @@
 
 PKG             := build-m4
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.4.19
-$(PKG)_CHECKSUM := b44b5c9746b69ee19204b7cb76d3a7b3eac69259
+$(PKG)_VERSION  := 1.4.20
+$(PKG)_CHECKSUM := 025df51430845178f926b20a078f994eefaa50fd
 $(PKG)_SUBDIR   := m4-$($(PKG)_VERSION)
 $(PKG)_FILE     := m4-$($(PKG)_VERSION).tar.xz
-$(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/m4/$($(PKG)_FILE)
+$(PKG)_URL      := https://ftp.gnu.org/gnu/m4/$($(PKG)_FILE)
 $(PKG)_DEPS     := 
 
 define $(PKG)_UPDATE
-    $(WGET) -q -O- 'http://ftp.gnu.org/gnu/m4/?C=M;O=D' | \
+    $(WGET) -q -O- 'https://ftp.gnu.org/gnu/m4/?C=M;O=D' | \
     $(SED) -n 's,.*<a href="m4-\([0-9\.]*\)\.tar.*,\1,p' | \
     $(SORT) -V |
     tail -1
