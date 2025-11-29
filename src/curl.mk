b53d08ab9582fcace6e2da50a5d4f74814817ae9
John Donoghue <john.donoghue@ieee.org>
2025-11-28 19:24:02 -0500

    * src/curl.mk: update to v8.17.0

--- src/curl.mk
+++ src/curl.mk
@@ -3,8 +3,8 @@
 
 PKG             := curl
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 8.16.0
-$(PKG)_CHECKSUM := 79a2145acd1e64cbad18aa87d2d9c007b64f8299
+$(PKG)_VERSION  := 8.17.0
+$(PKG)_CHECKSUM := 8859f774b7fef8dcbfd61848cbd336d15cf12773
 $(PKG)_SUBDIR   := curl-$($(PKG)_VERSION)
 $(PKG)_FILE     := curl-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := http://curl.haxx.se/download/$($(PKG)_FILE)
