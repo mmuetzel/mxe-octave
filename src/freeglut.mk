6cb7b3147803506ad640ae4d6f4e8cc0ea71af7b
John Donoghue <john.donoghue@ieee.org>
2025-11-28 19:26:36 -0500

    * src/freeglut.mk: update to v3.8.0

--- src/freeglut.mk
+++ src/freeglut.mk
@@ -3,8 +3,8 @@
 
 PKG             := freeglut
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.6.0
-$(PKG)_CHECKSUM := 1c5e572576b9a53c8c0616d9175146aa71425a0c
+$(PKG)_VERSION  := 3.8.0
+$(PKG)_CHECKSUM := 5fa9440f9fae331ad2bd881b3ffe74b30dbce84d
 $(PKG)_SUBDIR   := freeglut-$(word 1,$(subst -, ,$($(PKG)_VERSION)))
 $(PKG)_FILE     := freeglut-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://$(SOURCEFORGE_MIRROR)/project/freeglut/freeglut/$($(PKG)_VERSION)/$($(PKG)_FILE)
