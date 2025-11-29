519d0fc505e97031a0cd34f6ddceb2014eaf0cc3
John Donoghue <john.donoghue@ieee.org>
2025-11-28 19:31:57 -0500

    * src/geos.mk: update to v3.14.1

--- src/geos.mk
+++ src/geos.mk
@@ -3,8 +3,8 @@
 
 PKG             := geos
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.13.1
-$(PKG)_CHECKSUM := 92f3de2f5fc341981d7ffac3ef971d5be22cbe5e
+$(PKG)_VERSION  := 3.14.1
+$(PKG)_CHECKSUM := 1c5f3ca6d099fcedf85e030abbe7e3cc91231a55
 $(PKG)_SUBDIR   := geos-$($(PKG)_VERSION)
 $(PKG)_FILE     := geos-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://download.osgeo.org/geos/$($(PKG)_FILE)
