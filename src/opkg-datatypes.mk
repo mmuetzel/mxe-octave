9d016d13e49f31ab9326a640737fa0f7a5b28fe8
John Donoghue <john.donoghue@ieee.org>
2025-12-23 09:23:25 -0500

    * src/opkg-datatypes.mk: update to v1.1.4 (grafted from 8048eb4c44d9197cf017a9e4fd66def2ae0dc649)

--- src/opkg-datatypes.mk
+++ src/opkg-datatypes.mk
@@ -3,8 +3,8 @@
 
 PKG             := opkg-datatypes
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.1.3
-$(PKG)_CHECKSUM := 4b2121502c9e7f9c597221b048c438ffd143774e
+$(PKG)_VERSION  := 1.1.4
+$(PKG)_CHECKSUM := b4ec3233526870688f3f935501ccd469585b8cdd
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := datatypes-release-$($(PKG)_VERSION)
 $(PKG)_FILE     := datatypes-$($(PKG)_VERSION).tar.gz
