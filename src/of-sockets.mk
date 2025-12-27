3bbbfdcc6497d1cfaf521cc371993b501e95834e
John Donoghue <john.donoghue@ieee.org>
2025-12-27 15:01:49 -0500

    * src/of-sockets.mk: update to v1,5,0, use github repo

--- src/of-sockets.mk
+++ src/of-sockets.mk
@@ -3,12 +3,13 @@
 
 PKG             := of-sockets
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.4.1
-$(PKG)_CHECKSUM := 513d864abfc9d4afa2dc73a7ad5f158f402fd8f1
+$(PKG)_VERSION  := 1.5.0
+$(PKG)_CHECKSUM := 0c36b18221d16103c0a6566cbc94fafe99af59ad
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := sockets-$($(PKG)_VERSION)
 $(PKG)_FILE     := sockets-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
+$(PKG)_URL      := https://github.com/gnu-octave/octave-sockets/releases/download/release-$($(PKG)_VERSION)/$($(PKG)_SUBDIR).tar.gz
 $(PKG)_DEPS     := 
 
 ifeq ($(ENABLE_BINARY_PACKAGES),yes)
@@ -16,7 +17,7 @@ ifeq ($(ENABLE_BINARY_PACKAGES),yes)
 endif
 
 define $(PKG)_UPDATE
-    $(OCTAVE_FORGE_PKG_UPDATE)
+    $(call GITHUB_PKG_UPDATE,gnu-octave,octave-sockets,release-)
 endef
 
 define $(PKG)_BUILD
