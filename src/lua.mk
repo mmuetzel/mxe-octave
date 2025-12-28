222d14ed558f0c8bf4c38bb6c291ee3c2f6b3acf
John Donoghue <john.donoghue@ieee.org>
2025-12-28 08:01:45 -0500

    * src/lua.mk: update to v5.5.0

--- src/lua.mk
+++ src/lua.mk
@@ -3,8 +3,8 @@
 
 PKG             := lua
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.4.8
-$(PKG)_CHECKSUM := ced7c0ec99ad9d984ad3e7af5fcd2deef2b33f9f
+$(PKG)_VERSION  := 5.5.0
+$(PKG)_CHECKSUM := 71c930fa188fd244f353f227175c6f5ee5b52f6a
 $(PKG)_SUBDIR   := lua-$($(PKG)_VERSION)
 $(PKG)_FILE     := lua-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://www.lua.org/ftp/$($(PKG)_FILE)
