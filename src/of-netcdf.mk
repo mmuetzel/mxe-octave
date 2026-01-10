ee38f36282b32177781c45598986cb9cdbdd312a
John Donoghue <john.donoghue@ieee.org>
2026-01-10 18:10:31 -0500

    * src/of-netcdf.mk: update to v1.0.19, use github

--- src/of-netcdf.mk
+++ src/of-netcdf.mk
@@ -3,12 +3,12 @@
 
 PKG             := of-netcdf
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.0.18
-$(PKG)_CHECKSUM := ee871fd73f342a4070fc17f6c81227f503f3385e
+$(PKG)_VERSION  := 1.0.19
+$(PKG)_CHECKSUM := a69ebecf45f66fc67425544a9c2204019436c663
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := netcdf-$($(PKG)_VERSION)
 $(PKG)_FILE     := netcdf-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
+$(PKG)_URL      := https://github.com/gnu-octave/octave-netcdf/releases/download/v$($(PKG)_VERSION)/$($(PKG)_SUBDIR).tar.gz
 $(PKG)_DEPS     := netcdf
 
 ifeq ($(ENABLE_BINARY_PACKAGES),yes)
@@ -16,7 +16,7 @@ ifeq ($(ENABLE_BINARY_PACKAGES),yes)
 endif
 
 define $(PKG)_UPDATE
-    $(OCTAVE_FORGE_PKG_UPDATE)
+    $(call GITHUB_PKG_UPDATE,gnu-octave,octave-netcdf,v)
 endef
 
 define $(PKG)_BUILD
