7c84ca3a34a1b942da485e196143ead388dd6e2d
Markus Mützel <markus.muetzel@gmx.de>
2026-02-13 19:06:57 +0100

    sed: Update to 4.9
    * src/build-sed.mk: Update version and checksum.

--- src/build-sed.mk
+++ src/build-sed.mk
@@ -3,8 +3,8 @@
 
 PKG             := build-sed
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.8
-$(PKG)_CHECKSUM := 61bd770062d49cdab3f0f45df473e2bf950ba266
+$(PKG)_VERSION  := 4.9
+$(PKG)_CHECKSUM := 8ded1b543f1f558cbd5d7b713602f6a8ee84bde4
 $(PKG)_SUBDIR   := sed-$($(PKG)_VERSION)
 $(PKG)_FILE     := sed-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := ftp://ftp.gnu.org/pub/gnu/sed/$($(PKG)_FILE)
