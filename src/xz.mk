2d795812b486c8fc94a84cf1a553faa8b8a125fe
Markus Mützel <markus.muetzel@gmx.de>
2026-02-13 18:28:49 +0100

    xz: Update to 5.8.2
    * src/build-xz.mk, src/xz.mk: Update version and checksum.

--- src/xz.mk
+++ src/xz.mk
@@ -3,8 +3,8 @@
 
 PKG             := xz
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 5.8.1
-$(PKG)_CHECKSUM := c29df8058b6279bfab2744f3e79b2940229207a3
+$(PKG)_VERSION  := 5.8.2
+$(PKG)_CHECKSUM := 4b4b3c2cd242874929a11e4ff57498ef7efdcd1a
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://github.com/tukaani-project/xz/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
