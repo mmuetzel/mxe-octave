a09bd506ab5672bbee0dc7b64d4216106898bd54
John Donoghue <john.donoghue@ieee.org>
2026-02-15 18:41:11 -0500

    * src/openjpeg.mk: update to v2.5.4

--- src/openjpeg.mk
+++ src/openjpeg.mk
@@ -3,8 +3,8 @@
 
 PKG             := openjpeg
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.4.0
-$(PKG)_CHECKSUM := bbbf4dc4d9ce95286843cd39ac2febd3fd516c9d
+$(PKG)_VERSION  := 2.5.4
+$(PKG)_CHECKSUM := c299b70e495e74d858f6d22051de1d5a01a64d33
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/uclouvain/$(PKG)/archive/v$($(PKG)_VERSION).tar.gz
@@ -19,7 +19,6 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && cmake \
         $($(PKG)_CMAKE_FLAGS) \
-        -DBUILD_PKGCONFIG_FILES=ON \
         -DBUILD_TESTING=OFF \
         $(CMAKE_CCACHE_FLAGS) \
         $(CMAKE_BUILD_SHARED_OR_STATIC) \
@@ -28,4 +27,6 @@ define $(PKG)_BUILD
 
     $(MAKE) -C '$(1)' -j '$(JOBS)' VERBOSE=1
     $(MAKE) -C '$(1)' -j '1' VERBOSE=1 DESTDIR='$(3)' install
+
+
 endef
