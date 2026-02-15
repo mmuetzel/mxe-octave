9b36b4fffd174084fab7864cad61fdd0b6d5e8c9
John Donoghue <john.donoghue@ieee.org>
2026-02-15 16:49:51 -0500

    opkg-coder: update to v1.11.1
    * src/opkg-coder.mk: update version, checksum
    
    * dist-files.mk: update for removed patches
    
    * src/opkg-coder-1-use-Makefile.patch,
      src/opkg-coder-2-no-additional-double-quotes.patch
      : removed files

--- src/opkg-coder.mk
+++ src/opkg-coder.mk
@@ -3,8 +3,8 @@
 
 PKG             := opkg-coder
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.10.1
-$(PKG)_CHECKSUM := a7deff40b26a1f607b0618029bc94d7d3aaf7b75
+$(PKG)_VERSION  := 1.11.1
+$(PKG)_CHECKSUM := f067c00a3de867d8f8c457a9d96885c0a66feb3b
 $(PKG)_SUBDIR   := OctaveCoder-coder-$($(PKG)_VERSION)
 $(PKG)_FILE     := coder-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/shsajjadi/OctaveCoder/releases/download/coder-$($(PKG)_VERSION)/$($(PKG)_FILE)
