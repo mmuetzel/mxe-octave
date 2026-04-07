32fa185fb65d0b4c905ab69cb818e546863fba19
Markus Mützel <markus.muetzel@gmx.de>
2026-04-07 10:21:37 +0200

    opkg-mqtt: Fix download link.
    * src/opkg-mqtt.mk: Fix download link. The old link no longer works.

--- src/opkg-mqtt.mk
+++ src/opkg-mqtt.mk
@@ -7,7 +7,7 @@ $(PKG)_VERSION  := 0.0.6
 $(PKG)_CHECKSUM := 6d3fed2f41a5b27941a206fc00e0e03a8515f683
 $(PKG)_SUBDIR   := octave-mqtt-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
-$(PKG)_URL      := https://github.com/gnu-octave/octave-mqtt/releases/download/release-$($(PKG)_VERSION)/$($(PKG)_SUBDIR).tar.gz
+$(PKG)_URL      := https://github.com/gnu-octave/octave-mqtt/releases/download/release-$($(PKG)_VERSION)-main/$($(PKG)_SUBDIR).tar.gz
 $(PKG)_DEPS     := paho-mqtt-c
 
 ifeq ($(ENABLE_BINARY_PACKAGES),yes)
