cf399c43bd12b5c16f34d24a35e535bc924fb89e
John Donoghue <john.donoghue@ieee.org>
2026-05-09 17:39:24 -0400

    * src/of-nan.mk: update to v3.7.2, update downlod url

--- src/of-nan.mk
+++ src/of-nan.mk
@@ -3,12 +3,12 @@
 
 PKG             := of-nan
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.7.0
-$(PKG)_CHECKSUM := c504fc5403a78dc90de4a294bb6745561ae0e69e
+$(PKG)_VERSION  := 3.7.2
+$(PKG)_CHECKSUM := ab60e3f3fceff15b6bc25badcb9ca3d312c51216
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := nan-$($(PKG)_VERSION)
 $(PKG)_FILE     := nan-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := '$(OCTAVE_FORGE_BASE_URL)/$($(PKG)_FILE)/download'
+$(PKG)_URL      := 'https://pub.ista.ac.at/~schloegl/matlab/NaN/$($(PKG)_FILE)'
 $(PKG)_DEPS     := 
 
 ifeq ($(ENABLE_BINARY_PACKAGES),yes)
