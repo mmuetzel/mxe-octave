3ab7e5160d1dae07a165a35460c937d940805093
John Donoghue <john.donoghue@ieee.org>
2026-05-09 09:09:33 -0400

    * src/lcms.mk: update to v2.19.1, use github

--- src/lcms.mk
+++ src/lcms.mk
@@ -3,11 +3,11 @@
 
 PKG             := lcms
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.18
-$(PKG)_CHECKSUM := 3917db04b8d19b62ea6c9d8f4ff0404d145aca4a
-$(PKG)_SUBDIR   := $(PKG)$(word 1,$(subst ., ,$($(PKG)_VERSION)))-$(subst a,,$($(PKG)_VERSION))
-$(PKG)_FILE     := $(PKG)$(word 1,$(subst ., ,$($(PKG)_VERSION)))-$($(PKG)_VERSION).tar.gz
-$(PKG)_URL      := https://$(SOURCEFORGE_MIRROR)/project/$(PKG)/$(PKG)/$(call SHORT_PKG_VERSION,$(PKG))/$($(PKG)_FILE)
+$(PKG)_VERSION  := 2.19.1
+$(PKG)_CHECKSUM := b66f8317db40a775cb46cc465f66eb841c11aebf
+$(PKG)_SUBDIR   := $(PKG)2-$($(PKG)_VERSION)
+$(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
+$(PKG)_URL      := https://github.com/mm2/Little-CMS/releases/download/$(PKG)$($(PKG)_VERSION)/$($(PKG)_FILE)
 $(PKG)_DEPS     := jpeg tiff zlib
 
 define $(PKG)_UPDATE
