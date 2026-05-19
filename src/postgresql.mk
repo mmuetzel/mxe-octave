a55d8e04961503f018c3c55e6e11ee99ab27b5a7
John Donoghue <john.donoghue@ieee.org>
2026-05-18 21:02:11 -0400

    * src/postgresql.mk: update to v14.23

--- src/postgresql.mk
+++ src/postgresql.mk
@@ -3,8 +3,8 @@
 
 PKG             := postgresql
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 14.22
-$(PKG)_CHECKSUM := 1bcc50b8770aa67d02d6ede660cdc2fb26c6be00
+$(PKG)_VERSION  := 14.23
+$(PKG)_CHECKSUM := 03f22c0d1905d37699b795a9a9ac034dfad25bbc
 $(PKG)_SUBDIR   := postgresql-$($(PKG)_VERSION)
 $(PKG)_FILE     := postgresql-$($(PKG)_VERSION).tar.bz2
 $(PKG)_URL      := http://ftp.postgresql.org/pub/source/v$($(PKG)_VERSION)/$($(PKG)_FILE)
