94b71ad3bd84efd4539f0f29f345872656c2373f
John Donoghue <john.donoghue@ieee.org>
2026-06-07 16:54:40 -0400

    * src/of-control.mk: update to v4.2.2 (grafted from 10c0f2d24a1946148e4c6f495d3526a51cc9c031)

--- src/of-control.mk
+++ src/of-control.mk
@@ -3,8 +3,8 @@
 
 PKG             := of-control
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 4.2.1
-$(PKG)_CHECKSUM := 0e575532ab315a343690567068786b0e1070ef43
+$(PKG)_VERSION  := 4.2.2
+$(PKG)_CHECKSUM := 606cf3786ef01733d5dc859e52a1062e3c00ff68
 $(PKG)_REMOTE_SUBDIR := 
 $(PKG)_SUBDIR   := control-$($(PKG)_VERSION)
 $(PKG)_FILE     := control-$($(PKG)_VERSION).tar.gz
