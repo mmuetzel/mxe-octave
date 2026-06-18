45d44e54212b5f6efcd3ee804f8ea60e56ff5f2c
Markus Mützel <markus.muetzel@gmx.de>
2026-06-18 10:17:53 +0200

    mcfgthread: update to 2.4-ga.1
    * src/mcfgthread.mk: Update version and checksum.

--- src/mcfgthread.mk
+++ src/mcfgthread.mk
@@ -3,8 +3,8 @@
 
 PKG             := mcfgthread
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 2.3-ga.2
-$(PKG)_CHECKSUM := 758a62d2cfcf0a070e3d4f972593b213bdf586bb
+$(PKG)_VERSION  := 2.4-ga.1
+$(PKG)_CHECKSUM := c7277ce5f94465fc4c64829c59c276391f8f33cf
 $(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
 $(PKG)_FILE     := $($(PKG)_SUBDIR).tar.gz
 $(PKG)_URL      := https://github.com/lhmouse/$(PKG)/archive/v$($(PKG)_VERSION).tar.gz
