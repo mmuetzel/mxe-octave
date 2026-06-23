ffd66a9e89ba1f4fb959a94a022ed3bb9c61d942
John Donoghue <john.donoghue@ieee.org>
2026-06-23 07:52:42 -0400

    * src/gdcm.mk: update to v3.2.8

--- src/gdcm.mk
+++ src/gdcm.mk
@@ -3,8 +3,8 @@
 
 PKG             := gdcm
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 3.2.7
-$(PKG)_CHECKSUM := 2388cf8b94fb775893bdbc3029a773c8e44b4b89
+$(PKG)_VERSION  := 3.2.8
+$(PKG)_CHECKSUM := 03aa6e09af0ba53412ba760c5162bbf814c35f22
 $(PKG)_SUBDIR   := GDCM-$($(PKG)_VERSION)
 $(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/malaterre/GDCM/archive/v$($(PKG)_VERSION).tar.gz
