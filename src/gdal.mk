995f05f8d65963d645dd223b4e8f210072e736ac
John Donoghue <john.donoghue@ieee.org>
2026-07-27 15:12:39 -0400

    * src/gdal.mk: update to v3.13.2

--- src/gdal.mk
+++ src/gdal.mk
@@ -3,8 +3,8 @@
 
 PKG             := gdal
 $(PKG)_IGNORE   :
-$(PKG)_VERSION  := 3.13.1
-$(PKG)_CHECKSUM := df14e1d2aed38ab3ff81266f0a957f7c22b36954
+$(PKG)_VERSION  := 3.13.2
+$(PKG)_CHECKSUM := b9cc91f62b7ff8982493ec1d605ce6d92f8191c0
 $(PKG)_SUBDIR   := gdal-$($(PKG)_VERSION)
 $(PKG)_FILE     := gdal-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/OSGeo/gdal/archive/refs/tags/v$($(PKG)_VERSION).tar.gz
