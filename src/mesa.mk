31964ad263ee81c6bc2ec199f8546298fe1fbdfa
John Donoghue <john.donoghue@ieee.org>
2026-08-21 09:03:45 -0400

    * src/mesa.mk: update to v26.2.1

--- src/mesa.mk
+++ src/mesa.mk
@@ -2,8 +2,8 @@
 # See index.html for further information.
 
 PKG             := mesa
-$(PKG)_VERSION  := 26.1.7
-$(PKG)_CHECKSUM := aabcc46afdbe9e6243dc9547ed82e5038f5b47f1
+$(PKG)_VERSION  := 26.2.1
+$(PKG)_CHECKSUM := a5b335dee1944dd86874eccf1c53fd0ce618ba00
 $(PKG)_SUBDIR   := mesa-$($(PKG)_VERSION)
 $(PKG)_FILE     := mesa-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://archive.mesa3d.org/$($(PKG)_FILE)
