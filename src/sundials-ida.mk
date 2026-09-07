f0876d61ebe3e8fc0daee000ee2c44c59401067e
John Donoghue <john.donoghue@ieee.org>
2026-09-07 13:42:46 -0400

    * src/sundials-ida.mk: update to v7.9.0

--- src/sundials-ida.mk
+++ src/sundials-ida.mk
@@ -3,8 +3,8 @@
 
 PKG             := sundials-ida
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 7.8.0
-$(PKG)_CHECKSUM := 78b4234ff71b886ffd82cdde94e47081be2186c4
+$(PKG)_VERSION  := 7.9.0
+$(PKG)_CHECKSUM := 9372efb114a1f99cda40443834d91176a3679391
 $(PKG)_SUBDIR   := sundials-$($(PKG)_VERSION)
 $(PKG)_FILE     := sundials-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := https://github.com/LLNL/sundials/releases/download/v$($(PKG)_VERSION)/$($(PKG)_FILE)
